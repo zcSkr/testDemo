bf46516e13827dab014f9c629c896e6d04f23ceb
zhengchao <524843954@qq.com>
2023-05-04 18:05:17 +0800

    feat: 增加request请求对http链接的支持

--- src/app.jsx
+++ src/app.jsx
@@ -54,6 +54,9 @@ const errorHandler = (error) => {
 const headerInterceptor = (url, options) => {
   const timestamp = new Date().getTime()
   const rand = Math.floor(Math.random() * 100) //0-100随机整数
+  if (url.substr(0, 4) != 'http') {
+    url = (process.env.NODE_ENV === 'development' ? '/api' : requestUrl) + url;
+  }
   return {
     url,
     options: {
