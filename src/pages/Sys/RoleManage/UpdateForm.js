dc0c10f94be2bcdac82ec7ac11e5a4259147c869
zhengchao <524843954@qq.com>
2024-04-26 14:16:51 +0800

    优化模块选择为Cascader.Panel

--- src/pages/Sys/RoleManage/UpdateForm.js
+++ src/pages/Sys/RoleManage/UpdateForm.js
@@ -1,5 +1,7 @@
-import React, { useState, useRef } from 'react';
-import { ProForm, ProFormText, ProFormTextArea, ProFormCascader } from '@ant-design/pro-components';
+import React, { useState, useRef, useEffect } from 'react';
+import { ProForm, ProFormText, ProFormTextArea } from '@ant-design/pro-components';
+import { Cascader, Spin } from 'antd'
+import { useRequest } from '@umijs/max'
 
 import * as services_module from '@/services/sys/module';
 
@@ -17,6 +19,27 @@ const UpdateForm = ({
   });
 
   const formRef = useRef()
+
+  const { data, loading } = useRequest(() => services_module.queryTree({ pageSize: 0 }))
+  useEffect(() => {
+    if (data) {
+      //给Cascader造回显的数据，二维数组
+      const moduleIds = data.list.map(item => {
+        if (formVals.moduleIds?.includes(item.id)) { //一级存在
+          const children = item.children
+          const arr = [item.id] //只有一级id的小数组
+          if (!children.every(item => values.moduleIds.includes(item.id))) { //该一级只有部分子级存在
+            const childrenIds = children.filter(item => values.moduleIds.includes(item.id)).map(item => item.id)
+            return childrenIds.map(childId => arr.concat([childId]))
+          }
+          return [arr]
+        }
+      }).filter(item => item).reduce((prev, next) => prev.concat(next), [])
+      // console.log(moduleIds,'moduleIds')
+      setFormVals(prevStates => ({ ...prevStates, moduleTreeList: data.list }))
+      formRef.current.setFieldValue('moduleIds', moduleIds)
+    }
+  }, [data]);
 
   return (
     <ProForm
@@ -44,37 +67,20 @@ const UpdateForm = ({
         rules={[{ required: true }]}
         fieldProps={{ autoSize: { minRows: 2, maxRows: 6 }, maxLength: 500, allowClear: true, showCount: true }}
       />
-      <ProFormCascader
-        name="moduleIds"
-        label="模块"
-        rules={[{ required: true }]}
-        fieldProps={{
-          multiple: true,
-          showSearch: true,
-          expandTrigger: "hover",
-          fieldNames: { label: 'name', value: 'id' },
-        }}
-        request={async () => {
-          const { data } = await services_module.queryTree({ pageSize: 0 })
-          //给Cascader造回显的数据，二维数组
-          const moduleIds = data.list.map(item => {
-            if (formVals.moduleIds?.includes(item.id)) { //一级存在
-              const children = item.children
-              const arr = [item.id] //只有一级id的小数组
-              if (!children.every(item => values.moduleIds.includes(item.id))) { //该一级只有部分子级存在
-                const childrenIds = children.filter(item => values.moduleIds.includes(item.id)).map(item => item.id)
-                return childrenIds.map(childId => arr.concat([childId]))
-              }
-              return [arr]
-            }
-          }).filter(item => item).reduce((prev, next) => prev.concat(next), [])
-          // console.log(moduleIds,'moduleIds')
-          setFormVals(prevStates => ({ ...prevStates, moduleTreeList: data.list }))
-          formRef.current.setFieldValue('moduleIds',moduleIds)
-          return data.list
-        }}
-      />
-
+      <Spin spinning={loading}>
+        <ProForm.Item
+          name="moduleIds"
+          label="模块"
+          rules={[{ required: true }]}
+        >
+          <Cascader.Panel
+            multiple
+            expandTrigger="hover"
+            fieldNames={{ label: 'name', value: 'id' }}
+            options={data?.list}
+          />
+        </ProForm.Item>
+      </Spin>
     </ProForm>
   );
 };
